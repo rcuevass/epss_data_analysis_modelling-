c7873b5ab8fa85cc3250f8b60065ec6e96db676c
rcuevass <thatconsultingroger@gmail.com>
2025-04-20 15:37:47 -0400

    checiking in frist version of bash script to pull daily EPSS data. Removed unnecessary comment regarding possible bugs

--- extract_data.sh
+++ extract_data.sh
@@ -4,7 +4,7 @@ cp sample_file.csv.gz epss_scores_test.csv.gz
 gunzip epss_scores_test.csv.gz
 # remove first line of file
 sed -i 1d epss_scores_test.csv
-## add column with date ..
+## add column with date
 sed 's/\s*$/ 2023_04_01/' epss_scores_test.csv >> epss_scores_test_date.csv
 # Rename columns date name ....
 sed -i '1s/2023_04_01/date' epss_scores_test_date.csv
